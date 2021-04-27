3845b67f5e0ccefd19ddd8ca922842f254b0f198
Megan Day <meganeday7@gmail.com>
2021-04-26 21:08:27 -0500

    commit tree fc1b4144544563f7b51d226a83a67f4add91cf85

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2000, someValue < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (someValue > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  return (someCity == "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipValue){
   // Write your code here!
+  switch(tipValue) {
+    case "generous" : 
+      return "Thank you so much.";
+    case "not as generous" :
+      return "Thank you.";
+    default :
+      return "Bye.";
+  }
 }
